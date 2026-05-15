9299a6603afe3b1bea70a755cccff43381f92186
vibeforge1111 <vibeforge1111@gmail.com>
2026-05-15 15:28:01 +0400

    chore: repin installer after env coherence fix

--- scripts/install.ps1
+++ scripts/install.ps1
@@ -1,7 +1,7 @@
 param(
     [string]$Prefix = "$HOME\.spark",
     [string]$Source = "https://github.com/vibeforge1111/spark-cli",
-    [string]$Ref = "bc5b510affa1623e98f19e89b6e77a6fd93ad14f",
+    [string]$Ref = "e800af58a3ccf977c5277d448661b827968f9375",
     [string]$NodeVersion = "22.18.0",
     [string]$PythonVersion = "3.11",
     [string]$UvVersion = "0.11.7",
@@ -29,7 +29,7 @@ param(
 )
 
 $ErrorActionPreference = "Stop"
-$SparkCliReleaseName = "spark-cli-public-installer-2026-05-15-r1"
+$SparkCliReleaseName = "spark-cli-public-installer-2026-05-15-r2"
 $RefWasProvided = $PSBoundParameters.ContainsKey("Ref")
 $Script:InstallLockDir = ""
 $Script:PythonExe = ""
